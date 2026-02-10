c68a0fcb9b9a0a34aa70a95e5ab8a7e72e1306d3
Murewa <adebajomurewa@gmail.com>
2026-02-10 09:42:00 -0500

    Feb 10. class work

--- csce242contents/examples/03-js/js/script.js
+++ csce242contents/examples/03-js/js/script.js
@@ -9,5 +9,43 @@ document.getElementById("btn-show-message").onclick = () => {
 document.getElementById("a-click").onclick = (e) => {
     e.preventDefault(); //not go to the link's destinations
     console.log(e.currentTarget);
-    e.currentTarget.innerHTML = "Clciked";
+    e.currentTarget.innerHTML = "Clicked";
+}
+
+//sart band stop ball bouncing
+document.getElementById("btn-bounce").onclick = (e) => {
+    const ball = document.getElementById("ball");
+    if(e.currentTarget.innerHTML.toLowerCase()  == "start") {
+        e.currentTarget.innerHTML = "Stop";
+        ball.classList.add("bounce");
+    }
+    else
+    {
+        e.currentTarget.innerHTML = "Start";
+        ball.classList.remove("bounce");
+    }
+}
+
+//Plant Health Example
+document.getElementById("txt-num-days").onchange = (e) => {
+    const plantMessage = parseInt(e.currentTarget.value);
+    const p = document.getElementById("p-plant-message")
+
+    if(plantMessage < 0) {
+        p.innerHTML = "Yay! We were fed today!";
+    }
+    else if(plantMessage <= 2) {
+        p.innerHTML = "I'm a little thirsty";
+    }
+    else if( plantMessage <= 5) {
+        p.innerHTML = "I'm starting to wilt!";
+    }
+    else {
+        p.innerHTML = "You killed me : (";
+    }
+}
+
+//toggle nav
+document.getElementById("toggle-nav").onclick = () => {
+    document.querySelector("#main-nav ul").classList.toggle("hide-small");
 }
