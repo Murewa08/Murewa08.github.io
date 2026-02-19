f229c02040d6e6986b683584739f380f89750d95
Murewa <adebajomurewa@gmail.com>
2026-02-19 08:36:53 -0500

    Updated bubbles function

---
+++ csce242contents/assignments/javascript-loops/script.js
@@ -0,0 +1,19 @@
+window.onload = () => {
+    const bubble1 = document.getElementById("bubble-1");
+    const bubble2 = document.getElementById("bubble-2");
+    const bubble3 = document.getElementById("bubble-3");
+    const bubble4 = document.getElementById("bubble-4");
+    const bubble5 = document.getElementById("bubble-5");
+    const bubble6 = document.getElementById("bubble-6");
+    const bubble7 = document.getElementById("bubble-7");
+    const bubble8 = document.getElementById("bubble-8");
+
+    bubble1.classList.add("float");
+    bubble2.classList.add("float");
+    bubble3.classList.add("float");
+    bubble4.classList.add("float");
+    bubble5.classList.add("float");
+    bubble6.classList.add("float");
+    bubble7.classList.add("float");
+    bubble8.classList.add("float");
+}
