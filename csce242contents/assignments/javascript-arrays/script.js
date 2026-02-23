7c4e5f0118db9fa5dfafcb583279a4272f96671c
Murewa <adebajomurewa@gmail.com>
2026-02-22 19:54:11 -0500

    Updated Feb 19th class work

--- csce242contents/assignments/javascript-arrays/script.js
+++ csce242contents/assignments/javascript-arrays/script.js
@@ -1,26 +1,59 @@
-const showSongs = () => {
-    const resultDiv = document.getElementById("result");
+const resultDiv = document.getElementById("result");
+const videoDiv = document.getElementById("video-box");
+const btnDisney = document.getElementById("btn-disney");
+const btnHappy = document.getElementById("btn-happy");
+
+const showHappySongs = () => {
+    btnDisney.style.display = "none";
     const ulElem = document.createElement("ul");
     resultDiv.append(ulElem);
 
-    const happySongTitle = ["Happy by Pharrell Williams", "I'm Standing by Ellie Rills"];
-    /**const happySongVideo = ["<iframe https://www.youtube.com/embed/ZbZSe6N_BXs?si=dEKI1fhnpT8OWhzs title = YouTube video player frameborder= 0 allow= accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share referrerpolicy= strict-origin-when-cross-origin allowfullscreen></iframe>"];
-
-    const songsArray = {};
-
-    for(let i = 0; i< happySongTitle.length; i++)
-    {
-        songsArray[happySongTitle[i]] = happySongVideo[i];
-    }*/
+    const happySongTitle = ["Happy by Pharrell Williams", "Don't Stop Me Now by Queen", "Can't Stop This Feeling by Justin Timberlake", "Don't Worry Be Happy by Bobby McFerrin", "I'm Still Standing by Taron Egerton"];
+    
+    const happySongLink = [];
+    happySongLink["Happy by Pharrell Williams"] = "ZbZSe6N_BXs?si=6CSDKrtAa0D8bnig";
+    happySongLink["Don't Stop Me Now by Queen"] = "HgzGwKwLmgM?si=swMmA5UbRcPleF76";
+    happySongLink["Can't Stop This Feeling by Justin Timberlake"] = "ru0K8uYEZWw?si=33mMigfo63lFVfdM";
+    happySongLink["Don't Worry be Happy by Bobby McFerrin"] = "d-diB65scQU?si=NSuD7FxafbsQYRqF";
+    happySongLink["I'm Still Standing by Taron Egerton"] = "pHZneOidj9A?si=aMEdZLuwh7-fDBHH";
 
     for(let i in happySongTitle)
     {
         const liElem = document.createElement("li");
-        liElem.textContent = happySongTitle[i];
+        liElem.innerHTML = happySongTitle[i];
         ulElem.append(liElem);
     }
 };
 
+const HappyMusicVid = () => {
+    videoDiv.append(happySongLink["Happy by Pharrell Williams"]);
+}
+
+const showDisneySongs = () => {
+    btnHappy.style.display = "none";
+    const ulElem2 = document.createElement("ul");
+    resultDiv.append(ulElem2);
+
+    const disneySongTitle = ["I'm A Star from Wish", "Into The Unknown from Frozen II", "How Far I'll Go from Moana", "The Family Madrigal from Encanto", "Un Poco Loco from Coco"];
+    
+    const disneySongLink = [];
+    disneySongLink["I'm A Star from Wish"] = "V-ZSXJlBBrg?si=XgWfjXa8eW0Fazfr";
+    disneySongLink["Into The Unknown from Frozen II"] = "gIOyB9ZXn8s?si=499Kx-AcFtIIA8cg";
+    disneySongLink["How Far I'll Go from Moana"] = "cPAbx5kgCJo?si=eopCOgclPUKGDyDd";
+    disneySongLink["The Family Madrigal from Encanto"] = "Yp5nPGWWMh4?si=kW9EVQprV3Joa28d";
+    disneySongLink["Un Poco Loco from Coco"] = "yg8116aeD7E?si=v0tpmth0b-G_IbAG";
+
+    for(let i in disneySongTitle)
+    {
+        const liElem2 = document.createElement("li");
+        liElem2.textContent = disneySongTitle[i];
+        ulElem2.append(liElem2);
+    }
+
+}
+
 window.onload = () => {
-    document.getElementById("mood-selector").onclick = showSongs;
+    document.getElementById("btn-happy").onclick = showHappySongs;
+    document.getElementsByName("Happy By Pharrell Williams").onclick = HappyMusicVid;
+    document.getElementById("btn-disney").onclick = showDisneySongs;
 };
--- csce242contents/examples/03-js/js/loops.js
+++ csce242contents/examples/03-js/js/loops.js
@@ -1,10 +1,9 @@
 document.getElementById("btn-first-loop").onclick = () => {
     const ul = document.getElementById("ul-first-loop");
 
-    for(let i=0; i<10; i++)
-    {
+    for(let i = 0; i < 10; i++){
         const li = document.createElement("li");
-        li.innerHTML = `I'm the ${i+1} element`;
+        li.innerHTML = `I'm thie ${i+1} element`;
         ul.append(li);
     }
 };
@@ -21,62 +20,64 @@ document.getElementById("btn-count-range").onclick = () => {
         return;
     }
 
-    for(let i=startNumber; i < endNumber+1; i++)
-    {
+    for(let i = startNumber; i < endNumber + 1; i++){
         const p = document.createElement("p");
         p.innerHTML = i;
         divNumRange.append(p);
-        p.onlcick = () => {
-            document.getElementById("number-range").innerHTML = `You clicked the ${i}th item`;
+        p.onclick = () => {
+            document.getElementById("number-message").innerHTML = 
+                `You clicked the ${i}th item`;
         };
     }
+
 };
 
-document.getElementById("a-show-toy").onclick = (e) => {
-    e.preventDefault(); //Don't go to the link destination
+document.getElementById("a-show-toys").onclick = (e) => {
+    e.preventDefault(); //don't go to the link destination
     const toyList = document.getElementById("toy-list");
     toyList.innerHTML = "";
 
     const toys = ["fish", "guitar", "popsicle sticks", "rc cars", "shoe"];
 
     //traditional for loop
-    for(let i=0; i<toys.length; i++)
-    {
+    for(let i=0; i < toys.length; i++){
         const li = document.createElement("li");
         li.innerHTML = toys[i];
-        toyList.append(i);
+        toyList.append(li);
     }
 
     //second way preferred
-    //(toy, i) where i is a parameter index
-    toys.forEach((toy) => {
+    //(toy, i)
+    toys.forEach((toy)=>{
         const li = document.createElement("li");
         li.innerHTML = toy;
         toyList.append(li);
     });
 };
 
-//Associative Array
+//Associative array
 const toyPrices = [];
-toyPrices["fish"] = 2.99;
-toyPrices["guitar"] = 200;
-toyPrices["popsicle sticks"] = 0.10;
+toyPrices["fist"] = 2.99;
+toyPrices["guitar"] = 200.00;
+toyPrices["popsicle sticks"] = 0.1;
 toyPrices["rc car"] = 59.99;
 toyPrices["shoe"] = 49.99;
 
 for(let toy in toyPrices) {
     const toyTable = document.getElementById("toy-table");
-    const tr = document.getElementById("tr");
+    const tr = document.createElement("tr");
     toyTable.append(tr);
-    const tdToy = document.getElementById("td");
+    
+    const tdToy = document.createElement("td");
     tdToy.innerHTML = toy;
-    tr.append(tdToy); 
-
-    const tdPrice = document.getElementById("td");
+    tr.append(tdToy);
+    
+    const tdPrice = document.createElement("td");
     tdPrice.innerHTML = `$${toyPrices[toy]}`;
     tr.append(tdPrice);
-
+    
     tr.onclick = () => {
-        document.getElementById("p-toy-desc").innerHTML = `You want a ${toy} so ask your mom for $${toyPrices[toy]}`;
+        document.getElementById("p-toy-desc").innerHTML = 
+            `You want a ${toy} so ask your mom for $${toyPrices[toy]}`;
     };
 }
