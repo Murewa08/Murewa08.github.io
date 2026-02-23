bef897498a9698a49c144168f0bbb23a5e1789b8
Murewa <adebajomurewa@gmail.com>
2026-02-22 21:46:29 -0500

    Updated JS Arrays

--- csce242contents/assignments/javascript-arrays/script.js
+++ csce242contents/assignments/javascript-arrays/script.js
@@ -3,57 +3,96 @@ const videoDiv = document.getElementById("video-box");
 const btnDisney = document.getElementById("btn-disney");
 const btnHappy = document.getElementById("btn-happy");
 
-const showHappySongs = () => {
-    btnDisney.style.display = "none";
-    const ulElem = document.createElement("ul");
-    resultDiv.append(ulElem);
+// Use objects for mapping song title -> video id
+const happySongTitle = [
+  "Happy by Pharrell Williams",
+  "Don't Stop Me Now by Queen",
+  "Can't Stop This Feeling by Justin Timberlake",
+  "Don't Worry Be Happy by Bobby McFerrin",
+  "I'm Still Standing by Taron Egerton"
+];
 
-    const happySongTitle = ["Happy by Pharrell Williams", "Don't Stop Me Now by Queen", "Can't Stop This Feeling by Justin Timberlake", "Don't Worry Be Happy by Bobby McFerrin", "I'm Still Standing by Taron Egerton"];
-    
-    const happySongLink = [];
-    happySongLink["Happy by Pharrell Williams"] = "ZbZSe6N_BXs?si=6CSDKrtAa0D8bnig";
-    happySongLink["Don't Stop Me Now by Queen"] = "HgzGwKwLmgM?si=swMmA5UbRcPleF76";
-    happySongLink["Can't Stop This Feeling by Justin Timberlake"] = "ru0K8uYEZWw?si=33mMigfo63lFVfdM";
-    happySongLink["Don't Worry be Happy by Bobby McFerrin"] = "d-diB65scQU?si=NSuD7FxafbsQYRqF";
-    happySongLink["I'm Still Standing by Taron Egerton"] = "pHZneOidj9A?si=aMEdZLuwh7-fDBHH";
+const happySongLink = []; 
+  happySongLink["Happy by Pharrell Williams"] = "ZbZSe6N_BXs";
+  happySongLink["Don't Stop Me Now by Queen"] = "HgzGwKwLmgM";
+  happySongLink["Can't Stop This Feeling by Justin Timberlake"] = "ru0K8uYEZWw";
+  happySongLink["Don't Worry Be Happy by Bobby McFerrin"] = "d-diB65scQU";
+  happySongLink["I'm Still Standing by Taron Egerton"] = "pHZneOidj9A";
 
-    for(let i in happySongTitle)
-    {
-        const liElem = document.createElement("li");
-        liElem.innerHTML = happySongTitle[i];
-        ulElem.append(liElem);
-    }
-};
+const disneySongTitle = [
+  "I'm A Star from Wish",
+  "Into The Unknown from Frozen II",
+  "How Far I'll Go from Moana",
+  "The Family Madrigal from Encanto",
+  "Un Poco Loco from Coco"
+];
 
-const HappyMusicVid = () => {
-    videoDiv.append(happySongLink["Happy by Pharrell Williams"]);
+const disneySongLink = [];
+  disneySongLink["I'm A Star from Wish"] = "V-ZSXJlBBrg";
+  disneySongLink["Into The Unknown from Frozen II"] = "gIOyB9ZXn8s";
+  disneySongLink["How Far I'll Go from Moana"] = "cPAbx5kgCJo";
+  disneySongLink["The Family Madrigal from Encanto"] = "Yp5nPGWWMh4";
+  disneySongLink["Un Poco Loco from Coco"] = "yg8116aeD7E";
+
+const  showHappySongs = () => {
+  btnDisney.style.display = "none"; // optional: hide other button
+  resultDiv.innerHTML = "";         // clear any previous list
+  videoDiv.innerHTML = "";
+
+  const ulElem = document.createElement("ul");
+
+  for (let i = 0; i < happySongTitle.length; i++) {
+    const title = happySongTitle[i];
+    const liElem = document.createElement("li");
+
+    // create clickable paragraph (or use <button> if you prefer)
+    const link = document.createElement("p");
+    link.textContent = title;
+    link.style.cursor = "pointer";
+
+    // click handler uses the song title to look up the ID
+    link.onclick = () => {
+      const embededPortion = happySongLink[title]; // correct lookup
+      videoDiv.innerHTML = `<iframe src="https://www.youtube.com/embed/${embededPortion}" frameborder="0" allowfullscreen></iframe>`;
+      videoDiv.style.display = "block";
+    };
+
+    liElem.appendChild(link); // put the clickable text inside the li
+    ulElem.appendChild(liElem);
+  }
+
+  resultDiv.appendChild(ulElem); // append the whole list
 }
 
-const showDisneySongs = () => {
-    btnHappy.style.display = "none";
-    const ulElem2 = document.createElement("ul");
-    resultDiv.append(ulElem2);
+function showDisneySongs() {
+  btnHappy.style.display = "none";
+  resultDiv.innerHTML = "";
+  videoDiv.innerHTML = "";
+  videoDiv.style.display = "none";
 
-    const disneySongTitle = ["I'm A Star from Wish", "Into The Unknown from Frozen II", "How Far I'll Go from Moana", "The Family Madrigal from Encanto", "Un Poco Loco from Coco"];
-    
-    const disneySongLink = [];
-    disneySongLink["I'm A Star from Wish"] = "V-ZSXJlBBrg?si=XgWfjXa8eW0Fazfr";
-    disneySongLink["Into The Unknown from Frozen II"] = "gIOyB9ZXn8s?si=499Kx-AcFtIIA8cg";
-    disneySongLink["How Far I'll Go from Moana"] = "cPAbx5kgCJo?si=eopCOgclPUKGDyDd";
-    disneySongLink["The Family Madrigal from Encanto"] = "Yp5nPGWWMh4?si=kW9EVQprV3Joa28d";
-    disneySongLink["Un Poco Loco from Coco"] = "yg8116aeD7E?si=v0tpmth0b-G_IbAG";
+  const ulElem = document.createElement("ul");
 
-    for(let i in disneySongTitle)
-    {
-        const liElem2 = document.createElement("li");
-        liElem2.textContent = disneySongTitle[i];
-        ulElem2.append(liElem2);
-    }
+  for (let i = 0; i < disneySongTitle.length; i++) {
+    const title = disneySongTitle[i];
+    const liElem = document.createElement("li");
+    const link = document.createElement("p");
+    link.textContent = title;
+    link.style.cursor = "pointer";
 
+    link.onclick = () => {
+      const embededPortion = disneySongLink[title];
+      videoDiv.innerHTML = `<iframe src="https://www.youtube.com/embed/${embededPortion}" frameborder="0" allowfullscreen></iframe>`;
+      videoDiv.style.display = "block";
+    };
+
+    liElem.appendChild(link);
+    ulElem.appendChild(liElem);
+  }
+
+  resultDiv.appendChild(ulElem);
 }
 
 window.onload = () => {
-    document.getElementById("btn-happy").onclick = showHappySongs;
-    document.getElementsByName("Happy By Pharrell Williams").onclick = HappyMusicVid;
-    document.getElementById("btn-disney").onclick = showDisneySongs;
+  document.getElementById("btn-happy").onclick = showHappySongs;
+  document.getElementById("btn-disney").onclick = showDisneySongs;
 };
