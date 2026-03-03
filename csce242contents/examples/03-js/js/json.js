37cbac7d82ad901b86a12e9850abe6899749ee80
Murewa <adebajomurewa@gmail.com>
2026-03-03 09:43:39 -0500

    Mar. 3rd class work

---
+++ csce242contents/examples/03-js/js/json.js
@@ -0,0 +1,48 @@
+//json file parsing
+//https://github.com/portiaportia/portiaportia.github.io/blob/master/json/shoes.json
+
+const getFish = async() => {
+    const url = "https://portiaportia.github.io/json/fish.json";
+    const response = await fetch(url);
+    return response.json();
+};
+
+const showFish = async() => {
+    const fish = await getFish();
+    const fishListDiv = document.getElementById("fish-list");
+
+    fish.forEach((fish)=>{
+        const section = document.createElement("section");
+        section.classList.add("fish");
+        fishListDiv.append(section);
+
+        const h3 = document.createElement("h3");
+        h3.innerHTML = fish.name;
+        section.appendChild(h3);
+
+        const pLength = document.createElement("p");
+        pLength.innerHTML = `Length: ${fish.length}`;
+        section.appendChild(pLength);
+
+        const h4 = document.createElement("h4");
+        h4.innerHTML = "Colors: ";
+        section.append(h4);
+
+        const ul = document.createElement("ul");
+        section.append(ul);
+
+        //loop through the colors
+        fish.colors.forEach(()=>{
+            const li = document.createElement("li");
+            li.innerHTML = color;
+            ul.append(li);
+        });
+
+        const img = document.createElement("img");
+        section.prepend(img);
+        img.src = `https://portiaportia.github.io/json/${fish.img}`;
+    });
+};
+
+showFish();
+//https://openbrewerydb.org/
